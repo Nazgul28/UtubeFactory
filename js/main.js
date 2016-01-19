2a15142ae728bd8d64bbf0c6851649e626e39c1b
Nazgul28 <lfmassola@gmail.com>
2016-01-18 21:09:50 -0800

    changing scripts jquery

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// $(function () {
+	console.log("Hi there")
+
+
+for (var i =10; i>0; i--){
+	if (i%3 == 0) {
+		document.write("fizz");
+	} else if (i%5 == 0){
+		document.write("buzz");
+	}
+	else {
+	document.write(i);
+}
+}
